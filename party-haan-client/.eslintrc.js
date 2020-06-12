05ee29dd4456aedae5fda77624f05ab695bc3724
atvantage <icepanda501@gmail.com>
2020-06-12 15:08:17 +0700

    add prefer-pronise-reject

--- party-haan-client/.eslintrc.js
+++ party-haan-client/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'react/forbid-prop-types': 0,
     'react/prop-types': 0,
     'object-curly-newline': 0,
+    'prefer-promise-reject-errors' :0,
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
